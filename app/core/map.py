9b6ca8a41073355675f1ef918afd5f83a9f68d9e
E-werd <1765057+E-werd@users.noreply.github.com>
2024-11-20 13:15:00 -0500

    added debugging output

--- app/core/map.py
+++ app/core/map.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # External
-import logging
+import logging, time
 from PIL import Image, ImageFont, ImageDraw
 import numpy as np
-from scipy.ndimage import measurements
+from scipy.ndimage import label
 from pathlib import Path
 # Internal
 from app.core.player import Player
@@ -23,10 +23,12 @@ class Map:
         self.players: dict[str, Player] = {}
         self.levels: dict[str, LevelBase] = {}
         
+        # Check if output path set, if not set at input path
         if (self.out_image_path == None):
             out_stem: str = "out_" + self.in_image_path.stem
             self.out_image_path = self.in_image_path.with_stem(stem=out_stem).resolve()
 
+        # Load the input image
         logging.info(f"Loading map image from file: {self.in_image_path}")
         self.image: np.ndarray = self.__get_image_array(img_path=self.in_image_path)
 
@@ -70,24 +72,47 @@ class Map:
         :seed_point: (x,y) position inside a province
         :new_color: (r,g,b) value to fill'''
         logging.debug(f"Filling province at {str(seed_point)} with color {str(new_color)}")
+
+        tic = time.perf_counter()
         # Get color at the seed_point
         seed_color = self.image[seed_point[1], seed_point[0]] # Gets pixel in rgb at image coords [y, x]
+        toc = time.perf_counter()
+        logging.debug(f"Color time: {toc - tic:0.4f}s")
 
+        tic = time.perf_counter()
         # Generate a binary image (mask) where pixels with the seed color are white
         mask = np.all(self.image == seed_color, axis=-1)
+        toc = time.perf_counter()
+        logging.debug(f"Binary image time: {toc - tic:0.4f}s")
+        logging.debug(f"Binary image size: {np.array(object=mask).shape}")
 
+        tic = time.perf_counter()
         # Label all connected regions in the mask
-        labeled_array, num_features = measurements.label(mask)
+        labeled_array, num_features = label(input=mask)
+        toc = time.perf_counter()
+        logging.debug(f"Label1 time: {toc - tic:0.4f}s")
+        logging.debug(f"Label1 size: {np.array(object=labeled_array).shape}")
 
+        tic = time.perf_counter()
         # Get label of the region where seed_point is located
         seed_label = labeled_array[seed_point[1], seed_point[0]]
+        toc = time.perf_counter()
+        logging.debug(f"Label2 time: {toc - tic:0.4f}s")
+        logging.debug(f"Label2 size: {np.array(object=seed_label).shape}")
 
+        tic = time.perf_counter()
         # Create a new mask with only the selected region
         new_mask = (labeled_array == seed_label)
+        toc = time.perf_counter()
+        logging.debug(f"New mask time: {toc - tic:0.4f}s")
+        logging.debug(f"New mask size: {np.array(object=new_mask).shape}")
 
+        tic = time.perf_counter()
         # Generate a new image, replacing the selected region color
         new_img_array: np.ndarray = self.image.copy()
         new_img_array[new_mask] = new_color
+        toc = time.perf_counter()
+        logging.debug(f"New image time: {toc - tic:0.4f}s")
         
         self.image = new_img_array
 
